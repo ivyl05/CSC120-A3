303172fe979431923603e3e1b32cedb62dd76c34
Ivy <ivylichenke@gmail.com>
2024-09-27 23:59:17 -0400

    Submit

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,129 @@
+/*
+ * File name: Conversation.java
+ * Description: Create a Chatbot
+ * Author: Ivy Li
+ * Date: 27 September 2024
+ */
+
+import java.util.Scanner;
+
+import java.util.Random;
+
+
+/*
+ * class conversation creates a chatbot that is able to mirror certain words in the response or uses canned responses.
+ */
 class Conversation {
 
+  static String[] CannedResponse ={
+    "Mhm!",
+    "Interesting!",
+    "Tell me more about it"
+  };
+
+  static String[][] Mirror ={
+    {"i", "you"},
+    {"me", "you"},
+    {"am", "are"},
+    {"you", "I"},
+    {"my", "your"},
+    {"your", "my"}
+  };
+  static int rounds;
+  static String imput[];
+  
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    BeginConversation();
   }
+
+  /*
+   * Ask user how many rounds and store it into rounds
+   * return the number of rounds
+   */
+  public static int Rounds() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("How many rounds?");
+    int rounds = scanner.nextInt();
+    scanner.nextLine(); 
+    return rounds;
+
+  }
+
+  /*
+   * This method starts the conversation and store the conversation into transcript
+   * return nothing
+   */
+  public static void BeginConversation() {
+    int rounds = Rounds();
+    String[] transcript = new String[rounds]; 
+    Scanner scanner = new Scanner(System.in);
+    for (int i = 0; i < rounds; i++) {
+      // Scanner scanner = new Scanner(System.in);
+      System.out.println("You: ");
+      String imput = scanner.nextLine().toLowerCase(); 
+      String response = Response(imput, i);
+      transcript[i] = "You: " + imput + "\nBot: " + response;
+      System.out.println("Bot: " + response);
+      
+    }
+    scanner.close();
+    Transcript(transcript);
+    
+  }
+
+  /*
+   * This method randomize canned response although it didn't really work
+   * returns CannedResponse[]
+   */
+  public static String CannedResponse(){
+    Random random = new Random();
+    int num = random.nextInt(3);
+    return CannedResponse[num];
+  }
+
+  /*
+   * This method is generated with the help of Chat GPT
+   * This method generates response and determine whether words should be mirrored or should print out a canned response.
+   * Returns userResponse
+   */
+  public static String Response(String userInput, int rounds){
+    String userResponse = mirroredResponse(userInput);
+    if (userResponse.equals(userInput)){
+      userResponse=CannedResponse [rounds % CannedResponse.length];
+    }
+    return userResponse;
+
+  }
+
+  /*
+   * This method is generated with the help of Chat GPT
+   * This method generates mirrored words
+   */
+  public static String mirroredResponse(String userInput){
+    String[] words = userInput.split(" "); 
+        for (int i = 0; i < words.length; i++) {
+            for (String[] mirror : Mirror) {
+                if (words[i].equals(mirror[0])) {
+                    words[i] = mirror[1];
+                    break;
+                  }
+              }
+          }
+      return String.join(" ", words);
+    }
+
+/*
+ * This method print out the transcript in the end
+ * Returns nothing
+ */
+public static void Transcript(String[] transcript ){
+  System.out.println("\nConversation Transcript:");
+  for(String entry: transcript){
+    System.out.println(entry);
+  }
+}
+  
+
+
+  
 }
--- reflection.md
+++ reflection.md
@@ -5,3 +5,9 @@ Use this file to record your reflection on this assignment.
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+I did not consult with my classmates on this assignment, but I attended TA hours on Tuesday. Because I had some previous experience with Java, I am much more familiar with Java than Python. 
+
+The main difference between Java and Python when writing code is the syntax. Java is more “wordy” and less straightforward compared with Python. For example, Java requires System.out.println(); while Python only requires print(). Another difference is that Java uses a compiler, and Python is interpreted, which also means that Java is faster than Python. Some similarities are that both allow OOP and both are widely used.  
+
+The code was able to compile successfully. It displays the right amount of rounds and can printout the conversation transcript at the end. I wasn’t able to have it change the punctuation in the response. Random also did not work in my code. It is able to print out canned responses, but cannot randomize them. I would recommend other students to watch Youtube videos on Java Syntax if they are not familiar with Java, and they should start this assignment early so they have time to get help. 
